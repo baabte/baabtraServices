bef611723b92a06ed7c79f0e52cadb3efa610f1b
vineeth91 <vineeth@baabte.com>
2015-03-23 19:15:35 +0530

    modified template for sending  mail

--- myapp/emailSms.py
+++ myapp/emailSms.py
@@ -29,26 +29,36 @@ def sendEmailSmsNotification(request):  #this service will add & update course e
 
     if request.method == 'POST':
         try:
+
             stream = StringIO(request.body)
             data = JSONParser().parse(stream)
-            data['template']="An Account has been created for you in Baabtra.com"
-            data['Heading'] = "Welcome to Baabtra.com"
+            data['template']="A Mentee  Account has been created for you in " + data['userName']
+            data['Heading'] = "Welcome to " + data['userName']
             data['user']=data['menteeName']
-            data['LOGO_PATH']=settings.LOGO_PATH
+            #data['LOGO_PATH']=settings.LOGO_PATH
             message = get_template(settings.TEMPLATE_DIRS[0]+'/user_registration.html').render(Context(data))
-            send_mail('Welcome to Baabtra.com',message,settings.EMAIL_HOST_USER,[data['menteeEmail']],fail_silently=False,html_message=message)#for sending mail to mentee 
-            #print(message)
+            #send_mail('An account is created for you',message,settings.EMAIL_HOST_USER,[data['menteeEmail']],fail_silently=False,html_message=message)#for sending mail to mentee 
+            email= EmailMessage('An account is created for you',message,settings.EMAIL_HOST_USER,[data['menteeEmail']])
+            #email.attach_file(settings.LOGO_PATH+'Logo.png')
+            email.content_subtype = 'html'
+            email.send();
             data['user']=data['userName']
-            data['template'] ="Successfullly created a mentee account for "+data['menteeName'] +" in Baabtra.com"
+            data['template'] ="Successfullly created a mentee account for "+data['menteeName'] +" in "+ data['userName']
             message = get_template(settings.TEMPLATE_DIRS[0]+'/user_registration.html').render(Context(data))
-            send_mail('Welcome to Baabtra.com',message,settings.EMAIL_HOST_USER,[data['userEmail']],fail_silently=False,html_message=message)#for sending mail to mentee 
+            email= EmailMessage('New Account is registered',message,settings.EMAIL_HOST_USER,[data['userEmail']])
+            email.content_subtype = 'html'
+            email.send();
+            #send_mail('New Account is registered',message,settings.EMAIL_HOST_USER,[data['userEmail']],fail_silently=False,html_message=message)#for sending mail to company 
             for item in data['evaluatorEmailLIst']:
-                print item['Name']
-                print item['Email']['userName']
+                #print item['Name']
+                #print item['Email']['userName']
                 data['user']=item['Name']
-                data['template'] ="Successfullly created a mentee account for "+data['menteeName'] +" in Baabtra.com"
+                data['template'] ="A task has been assigned to you in "+data['userName'] 
                 message = get_template(settings.TEMPLATE_DIRS[0]+'/user_registration.html').render(Context(data))
-                send_mail('Welcome to Baabtra.com',message,settings.EMAIL_HOST_USER,[data['menteeEmail']],fail_silently=False,html_message=message)#for sending mail to mentee 
+                #send_mail('A task has been Assigned to you',message,settings.EMAIL_HOST_USER,[data['menteeEmail']],fail_silently=False,html_message=message)#for sending mail to mentee 
+                email= EmailMessage('A task has been Assigned to you',message,settings.EMAIL_HOST_USER,[item['Email']['userName']])
+                email.content_subtype = 'html'
+                email.send();
             #batchDetails=dbconn.system_js.fnAddNewBatches(data['batchObj'])    
         except ValueError:
             return Response(json.dumps(ValueError, default=json_util.default))
