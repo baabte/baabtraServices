94fa9406d3cdfcf084da4602cce77bd3a8e0866a
akshath4u <akshath@baabte.com>
2015-04-04 18:25:35 +0530

    commited chnages in custom form service

--- Djproject/urls.py
+++ Djproject/urls.py
@@ -197,7 +197,7 @@ urlpatterns = patterns('',
     url(r'^GetCode/$', 'myapp.commonCalls.GetCode', name='GetCode'),
     url(r'^setMenuType/$', 'myapp.globalSettings.setMenuType', name='setMenuType'),
     url(r'^fnLoadMenteesAttReport/$', 'myapp.reports.fnLoadMenteesAttReport', name='fnLoadMenteesAttReport'),
-     url(r'^saveMenuColor/$', 'myapp.globalSettings.saveMenuColor', name='saveMenuColor')
+    url(r'^saveMenuColor/$', 'myapp.globalSettings.saveMenuColor', name='saveMenuColor')
 
 )+ static('/files/', document_root=settings.FILEUPLOAD_PATH)
 
