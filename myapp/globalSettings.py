9d0647ee9f6e93d6c268d5aafa7ddaf2c26398e2
midhun123 <midhun@baabte.com>
2015-04-13 19:25:41 +0530

    attendance configuration

--- myapp/globalSettings.py
+++ myapp/globalSettings.py
@@ -262,3 +262,27 @@ def saveSubMenuAndBackgrounds(request):  #this service will save add and update 
         return Response(json.dumps(Resp, default=json_util.default))
     else:        
         return Response(json.dumps("failed", default=json_util.default))
+
+
+
+       
+#creater :Midhun Sudhakar
+@csrf_exempt
+@api_view(['GET','POST'])
+def saveAttendanceAlertSettings(request):  #this service will save add and update coures details
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            datas = data["data"]
+            Resp=dbconn.system_js.fnsaveAttendanceAlertSettings(datas)    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(Resp, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
