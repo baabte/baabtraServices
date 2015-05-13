3d8d8c50a039d6693faa65cbcd94203d8099637f
Lijin <lijin@baabte.com>
2015-05-13 19:39:07 +0530

    Added function for saving marksheet configuration

--- myapp/course.py
+++ myapp/course.py
@@ -401,5 +401,47 @@ def ExistingMaterialsView(request):  #this service will add & update course elem
         return Response(json.dumps("failed", default=json_util.default))
 
 
+#created by :Lijin
+@csrf_exempt
+@api_view(['GET','POST'])
+def getCourseSyllabus(request):
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            response = dbconn.system_js.fnGetCourseSyllabus(data['courseId'])    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(response, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+
+#created by :Lijin
+@csrf_exempt
+@api_view(['GET','POST'])
+def saveMarksheetElements(request):
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            response = dbconn.system_js.fnSaveMarksheetElements(data['courseId'],data['markSheetElements'])    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(response, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+
 
 
