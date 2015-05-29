c5440f7e6d9354f7f4c1dfa8b353e11084ad442a
jihin <jihin@baabte.com>
2015-05-30 03:36:21 +0530

    custom form by Jihin

--- myapp/custom-form.py
+++ myapp/custom-form.py
@@ -32,3 +32,105 @@ def fnLoadCustomFormTemplates(request):
     else:        
         return Response("failed")
 
+#creater :jihin
+@csrf_exempt
+@api_view(['GET','POST'])
+def fnSaveCustomFormMain(request):  #this service will add & update save Custom Form 
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            customForm = dbconn.system_js.fnSaveCustomFormMain(data['customForm'])    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(customForm, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+#creater :jihin
+@csrf_exempt
+@api_view(['GET','POST'])
+def fnLoadCustomFormsMain(request):  #this service will load Custom Forms
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            customForms = dbconn.system_js.fnLoadCustomFormMain()    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(customForms, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+
+#creater :jihin
+@csrf_exempt
+@api_view(['GET','POST'])
+def fnSaveCompanyCustomForm(request):  #this service will add & update save Custom Form 
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            customForm = dbconn.system_js.fnSaveCompanyCustomForm(data['customForm'])    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(customForm, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+#creater :jihin
+@csrf_exempt
+@api_view(['GET','POST'])
+def fnLoadCompanyCustomForm(request):  #this service will load Custom Forms
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            customForms = dbconn.system_js.fnLoadCompanyCustomForm(data['companyId'], data['formId'])    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(customForms, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+#creater :jihin
+@csrf_exempt
+@api_view(['GET','POST'])
+def fnLoadCustomFormforRegistration(request):  #this service will load Custom Forms
+    #connect to our local mongodb
+    db = Connection(settings.MONGO_SERVER_ADDR,settings.MONGO_PORT)
+    #get a connection to our database
+    dbconn = db[settings.MONGO_DB]
+
+    if request.method == 'POST':
+        try:
+            stream = StringIO(request.body)
+            data = JSONParser().parse(stream)
+            customForms = dbconn.system_js.fnLoadCustomFormforRegistration(data['companyId'], data['formId'])    
+        except ValueError:
+            return Response(json.dumps(ValueError, default=json_util.default))
+        return Response(json.dumps(customForms, default=json_util.default))
+    else:        
+        return Response(json.dumps("failed", default=json_util.default))
+
+
