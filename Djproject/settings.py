a971aeb4c8a7d6ecfced80b0f11e0bd0defa44b3
arunbaabte <arun@baabte.com>
2015-07-09 17:25:38 +0530

    commit to push the service change report generation

--- Djproject/settings.py
+++ Djproject/settings.py
@@ -77,16 +77,15 @@ WSGI_APPLICATION = 'Djproject.wsgi.application'
 
 
 
-# MONGO_SERVER_ADDR = 'localhost' #Mongodb address 
+MONGO_SERVER_ADDR = 'localhost' #Mongodb address 
 # MONGO_SERVER_ADDR = '192.168.2.20' #Mongodb address
-MONGO_SERVER_ADDR = '117.240.93.254' #Mongodb address
+# MONGO_SERVER_ADDR = '117.240.93.254' #Mongodb address
 # MONGO_SERVER_ADDR = '192.168.2.254' #Mongodb address
 # MONGO_SERVER_ADDR = '104.199.128.179' #Mongodb address cloud
 
 MONGO_PORT = 27017 #Mongodb port
 
-# MONGO_DB = 'baabtra_db'  #Database name
-MONGO_DB = 'preproduction'  #Database name
+MONGO_DB = 'baabtra_db'  #Database name
 
 FILEUPLOAD_PATH="uploaded" #path for uploading the files
 RESUME_PATH='http://localhost:8000/files/resume/' #path for mailing the resume link
